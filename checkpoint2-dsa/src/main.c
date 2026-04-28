f3154ec17d842ed14d05c9b901a960f792cc09cd
pearthureis <pearthur.reis@gmail.com>
2026-04-28 11:05:01 -0300

    Adicionei a função de calcular a derivada

--- checkpoint2-dsa/src/main.c
+++ checkpoint2-dsa/src/main.c
@@ -1,5 +1,25 @@
 #include<stdio.h>
 
+void calcularDerivada() {
+    // Para uma função simples: f(x) = ax^n -> f'(x) = (a*n)x^(n-1)
+    float coeficiente;
+    int expoente;
+
+    printf("\nPara a funcao f(x) = ax^n");
+    printf("\nDigite o coeficiente (a): ");
+    scanf("%f", &coeficiente);
+    printf("Digite o expoente (n): ");
+    scanf("%d", &expoente);
+    
+     if (expoente == 0) {
+        printf("Derivada: f'(x) = 0\n");
+    } else {
+        float novoCoeficiente = coeficiente * expoente;
+        int novoExpoente = expoente - 1;
+        printf("Derivada: f'(x) = %.2fx^%d\n", novoCoeficiente, novoExpoente);
+    }
+}
+
 int main() {
     
 
